99ea220f7191c1f5736320e543dbd6743ef318be
Taha Galata <tahagalata@protonmail.com>
2023-05-17 21:55:27 +0300

    Fixed the element not interractable for password.

--- scraping.py
+++ scraping.py
@@ -1,15 +1,15 @@
 from dotenv import dotenv_values
-
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 config = dotenv_values()
 
-driver = webdriver.Chrome()
-options = webdriver.ChromeOptions()
+driver = webdriver.Edge()
+options = webdriver.EdgeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Edge(options=options)
 
 driver.set_window_position(0, 0)
 driver.set_window_size(1920, 1080)
@@ -19,8 +19,6 @@ driver.get('https://www.upwork.com/ab/account-security/login')
 
 driver.find_element(By.ID,'login_username').send_keys(config["EMAIL"])
 driver.find_element(By.ID,'login_password_continue').click()
-driver.implicitly_wait(5)
+time.sleep(2)
 driver.find_element(By.ID,"login_password").send_keys(config["PASSWORD"])
 driver.find_element(By.ID,'login_control_continue').click()
-
-
