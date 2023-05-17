650f9a7058b068ee91c4ed48bc80b1f05cdc7198
Taha Galata <tahagalata@protonmail.com>
2023-05-18 00:55:04 +0300

    Added code to get categories

--- scraping.py
+++ scraping.py
@@ -2,23 +2,29 @@ from dotenv import dotenv_values
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 
 config = dotenv_values()
 
-driver = webdriver.Edge()
+# INITIALIZE EDGE BROWSER WEB DRIVER WITH REMOTE DEBUGGER
 options = webdriver.EdgeOptions()
-options.add_experimental_option("detach", True)
+options.add_experimental_option("debuggerAddress", "127.0.0.1:9014")
 driver = webdriver.Edge(options=options)
 
-driver.set_window_position(0, 0)
-driver.set_window_size(1920, 1080)
-
 # LOGIN
-driver.get('https://www.upwork.com/ab/account-security/login')
+driver.get('https://www.upwork.com/browse/bench')
 
-driver.find_element(By.ID,'login_username').send_keys(config["EMAIL"])
-driver.find_element(By.ID,'login_password_continue').click()
+driver.find_element(By.ID,"login_username").send_keys(config["EMAIL"])
+driver.find_element(By.ID,"login_password_continue").click()
 time.sleep(2)
 driver.find_element(By.ID,"login_password").send_keys(config["PASSWORD"])
-driver.find_element(By.ID,'login_control_continue').click()
+driver.find_element(By.ID,"login_control_continue").click()
+
+# GET CATEGORIES:
+time.sleep(3)
+driver.execute_script("window.scrollTo(0, 1920)")
+time.sleep(2)
+category_buttons = driver.find_elements(By.CSS_SELECTOR, ".categories > .mb-5 > button")
+categories = [btn.text for btn in category_buttons]
+
+# GET SUB-CATEGORIES
